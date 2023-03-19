e53de0e872f6525a09ad08e8190f9f7708f6d8ab
Said Gaforov <sgaforov@gmail.com>
2023-03-18 21:38:28 -0600

    Parametirizing xml test runs, now you can run any test smoke, regression from command line

--- src/test/java/com/exelenter/utils/Constants.java
+++ src/test/java/com/exelenter/utils/Constants.java
@@ -3,6 +3,7 @@ package com.exelenter.utils;
 public class Constants {
     public static final String CHROME_DRIVER_PATH = System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver";
     public static final String GECKO_DRIVER_PATH = System.getProperty("user.dir") + "/src/test/resources/drivers/geckodriver";
+    public static final String EDGE_DRIVER_PATH = System.getProperty("user.dir") + "/src/test/resources/drivers/msedgedriver";
     public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir") + "/src/test/resources/configs/configuration.properties";
     public static final int IMPLICIT_WAIT_TIME = 1;
     public static final int EXPLICIT_WAIT_TIME = 40;
